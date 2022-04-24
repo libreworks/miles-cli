9716e558c8c25c6493238e04346849de2df01b8d
Jonathan Hawk <jonathan@hawk.ninja>
2022-04-23 23:24:44 -0400

    chore: more unit test coverage

--- test/auth/cognito/facade.spec.js
+++ test/auth/cognito/facade.spec.js
@@ -87,10 +87,121 @@ describe("CognitoFacade", () => {
       const userPool = sinon.createStubInstance(CognitoUserPool);
       userPool.getCurrentUser.returns(cognitoUser);
       const cognitoStorage = new CognitoStorage();
-      const facadeResult = { incomplete: false, user: cognitoUser };
       const obj = new CognitoFacade(userPool, cognitoStorage);
       const result = obj.logOut();
       assert.ok(result instanceof Promise);
+    });
+    it("should throw an error without a session user", async () => {
+      const userPool = sinon.createStubInstance(CognitoUserPool);
+      userPool.getCurrentUser.returns(null);
+      const cognitoStorage = new CognitoStorage();
+      const obj = new CognitoFacade(userPool, cognitoStorage);
+      assert.throws(() => obj.logOut(), {
+        name: "Error",
+        message: "There is no user to log out",
+      });
+    });
+    it("should resolve to undefined", async () => {
+      const cognitoUser = sinon.createStubInstance(CognitoUser);
+      cognitoUser.signOut.callsFake((fn) => fn());
+      const userPool = sinon.createStubInstance(CognitoUserPool);
+      userPool.getCurrentUser.returns(cognitoUser);
+      const cognitoStorage = new CognitoStorage();
+      const obj = new CognitoFacade(userPool, cognitoStorage);
+      const result = await obj.logOut();
+      assert.strictEqual(result, undefined);
+    });
+    it("should reject if error sent from cognito", async () => {
+      const cognitoUser = sinon.createStubInstance(CognitoUser);
+      const err = new Error("An example Cognito error");
+      cognitoUser.signOut.callsFake((fn) => fn(err));
+      const userPool = sinon.createStubInstance(CognitoUserPool);
+      userPool.getCurrentUser.returns(cognitoUser);
+      const cognitoStorage = new CognitoStorage();
+      const obj = new CognitoFacade(userPool, cognitoStorage);
+      await assert.rejects(() => obj.logOut(), err);
+    });
+  });
+  describe("#confirmNewPassword", () => {
+    it("should return a Promise", async () => {
+      const cognitoUser = sinon.createStubInstance(CognitoUser);
+      cognitoUser.completeNewPasswordChallenge.callsFake(
+        (password, attributes, callbackObject) => {}
+      );
+      const userPool = sinon.createStubInstance(CognitoUserPool);
+      const cognitoStorage = new CognitoStorage();
+      const obj = new CognitoFacade(userPool, cognitoStorage);
+      const password = "Password123";
+      const attributes = ["username"];
+      sinon
+        .stub(obj, "createCallbackObject")
+        .callsFake((user, resolve, reject) => resolve({}));
+      const result = obj.confirmNewPassword(cognitoUser, password, attributes);
+      assert.ok(result instanceof Promise);
+    });
+    it("should resolve to a result", async () => {
+      const cognitoUser = sinon.createStubInstance(CognitoUser);
+      cognitoUser.completeNewPasswordChallenge.callsFake(
+        (password, attributes, callbackObject) => {}
+      );
+      const userPool = sinon.createStubInstance(CognitoUserPool);
+      const cognitoStorage = new CognitoStorage();
+      const obj = new CognitoFacade(userPool, cognitoStorage);
+      const password = "Password123";
+      const attributes = ["username"];
+      const facadeResult = { incomplete: true };
+      sinon
+        .stub(obj, "createCallbackObject")
+        .callsFake((user, resolve, reject) => resolve(facadeResult));
+      const result = await obj.confirmNewPassword(
+        cognitoUser,
+        password,
+        attributes
+      );
+      assert.strictEqual(result, facadeResult);
+    });
+  });
+  describe("#confirmPasswordReset", () => {
+    it("should return a Promise", async () => {
+      const cognitoUser = sinon.createStubInstance(CognitoUser);
+      cognitoUser.confirmPassword.callsFake(
+        (verificationCode, password, callbackObject) => {}
+      );
+      const userPool = sinon.createStubInstance(CognitoUserPool);
+      const cognitoStorage = new CognitoStorage();
+      const obj = new CognitoFacade(userPool, cognitoStorage);
+      const password = "Password123";
+      const verificationCode = "123456";
+      sinon
+        .stub(obj, "createCallbackObject")
+        .callsFake((user, resolve, reject) => resolve({}));
+      const result = obj.confirmPasswordReset(
+        cognitoUser,
+        verificationCode,
+        password
+      );
+      assert.ok(result instanceof Promise);
+    });
+    it("should resolve to a result", async () => {
+      const cognitoUser = sinon.createStubInstance(CognitoUser);
+      cognitoUser.confirmPassword.callsFake(
+        (verificationCode, password, callbackObject) => {}
+      );
+      const userPool = sinon.createStubInstance(CognitoUserPool);
+      const cognitoStorage = new CognitoStorage();
+      const obj = new CognitoFacade(userPool, cognitoStorage);
+      const password = "Password123";
+      const verificationCode = "123456";
+      const facadeResult = { incomplete: true };
+      sinon
+        .stub(obj, "createCallbackObject")
+        .callsFake((user, resolve, reject) => resolve(facadeResult));
+      const result = await obj.confirmPasswordReset(
+        cognitoUser,
+        verificationCode,
+        password
+      );
+      assert.strictEqual(result, facadeResult);
     });
   });
 });
